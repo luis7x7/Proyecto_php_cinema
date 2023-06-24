706df4f456819f86f84595a06524776ef3c3899d
luis <nieblesquinaluisangel@gmail.com>
2023-06-24 18:38:46 -0500

    nuevos cambios a la arquitectura

---
+++ app/views/inicio.php
@@ -0,0 +1,32 @@
+
+     <div>
+          <navbar class="navbar">
+               <img src="" alt="">
+               <ul>
+                    <li>
+                         <a href="">inicio</a>
+                    </li>
+                    <li>
+                         <a href="">genero</a>
+                    </li>
+                    <li>
+                         <a href="">categoria</a>
+                    </li>
+                    <li>
+                         <a href=""><img src="../img/carrito.png" alt=""></a>
+                    </li>
+
+               </ul>
+               <form action="">
+                    <input type="search">
+                    <button>buscar</button>
+               </form>
+
+
+          </navbar>
+          <h2> cinema</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam perferendis voluptatibus natus eveniet, totam
+               dolorem. Architecto nesciunt autem cupiditate, ipsa repudiandae labore laboriosam? Pariatur voluptatem
+               expedita provident? Laborum, praesentium dicta!</p>
+     </div>
+
